5fbd14e0a0674c2877594d9b11e1ff10d6f5ecf0
LudmilaBash <ludmiladvmnt@gmail.com>
2018-09-23 15:12:34 -0700

    15 spirographs now
    Added more

--- js/spiroart.js
+++ js/spiroart.js
@@ -1,7 +1,9 @@
 
 
 var AnimationFrameID;
-spirnum=6;
+spirnum=15;
+
+cycle=10000 // cycle of  seconds
 	
 window.requestAnimFrame = (function(callback) {
         return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
@@ -11,30 +13,33 @@ window.requestAnimFrame = (function(callback) {
         };
       })();
 
+
 function animate(startTime)
 {
   var time = (new Date()).getTime() - startTime;
   
-  var cycletime=time%(30000*spirnum); // cycle of 180 seconds
+  var cycletime=time%(cycle*spirnum); 
   
+
 for (var i=1; i<=spirnum; i++)
 {
-	if(cycletime>(i-1)*30000 && cycletime<=i*30000)
+
+	if(cycletime>(i-1)*cycle && cycletime<=i*cycle)
 	{
-		if(cycletime<(i-1)*30000 +500)      //clean non-current shapes
-			for (var j=1; j<=spirnum; j++)   
+		var sp=document.getElementById('sp-'+ i);
+		var spnext=document.getElementById('sp-'+ (i%spirnum +1));
+		if(cycletime<(i-1)*cycle +500)
+			for (var j=1; j<=spirnum; j++)   //clean non-current shapes
 			{
 				if(i==j)
 					continue;
 				var spj=document.getElementById('sp-'+ j);
 				spj.style.display="none";
 			}
-			
-		var sp=document.getElementById('sp-'+ i);
-		var spnext=document.getElementById('sp-'+ (i%spirnum +1));
-		if(cycletime>i*30000 -2000)
+				
+		if(cycletime>i*cycle -1500)
         {
-        	var opacity= (cycletime-(i*30000-2000))/2000;
+        	var opacity= (cycletime-(i*cycle-2000))/2000;
         	sp.style.opacity = 1-opacity;
         	spnext.style.opacity = opacity;
 			spnext.style.display="block";
@@ -45,7 +50,12 @@ for (var i=1; i<=spirnum; i++)
 				spnext.style.opacity=1;
 			}
         }				
-	}	
+	}
+    else
+	{
+		sp=spnext;
+		spnext=document.getElementById('sp-'+ (Math.floor(Math.random() * spirnum) + 1));
+	}
 }
  
 
