6bb402f0f065cef19637049e4b749659f0909093
LudmilaBash <ludmiladvmnt@gmail.com>
2018-09-24 18:26:19 -0700

    more time to set other shapes invisible

--- js/spiroart.js
+++ js/spiroart.js
@@ -31,18 +31,18 @@ for (var i=1; i<=spirnum; i++)
 	{
 		var sp=sps[i-1];
 		var spnext=sps[i%spirnum];
-		if(cycletime<(i-1)*cycle +500)
+		if(cycletime<(i-1)*cycle +1100)
 			for (var j=1; j<=spirnum; j++)   //clean non-current shapes
 			{
 				if(i==j)
 					continue;
-				var spj=document.getElementById('sp-'+ j);
+				var spj=sps[j-1];
 				spj.style.display="none";
 			}
 				
-		if(cycletime>i*cycle -1500)
+		if(cycletime>i*cycle -1200)
         {
-        	var opacity= (cycletime-(i*cycle-1500))/1500;
+        	var opacity= (cycletime-(i*cycle-1200))/1200;
         	sp.style.opacity = 1-opacity;
         	spnext.style.opacity = opacity;
 			spnext.style.display="block";
