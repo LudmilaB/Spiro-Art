255cab4ad8d5cc4fc210bcada3e700edf3748651
LudmilaBash <ludmiladvmnt@gmail.com>
2018-09-24 19:40:28 -0700

    Changed dissapearing

--- js/spiroart.js
+++ js/spiroart.js
@@ -6,7 +6,7 @@ var sps=[];
 for (var i=0; i<spirnum; i++)
 	  sps[i]=document.getElementById('sp-'+ (i+1));
 
-cycle=12000 // cycle of  seconds
+cycle=15000 // cycle of  seconds
 	
 window.requestAnimFrame = (function(callback) {
         return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
@@ -19,44 +19,32 @@ window.requestAnimFrame = (function(callback) {
 
 function animate(startTime)
 {
-  var time = (new Date()).getTime() - startTime;
-  
-  var cycletime=time%(cycle*spirnum); 
-  
+var time = (new Date()).getTime() - startTime; 
+var cycletime=time%(cycle*spirnum); 
 
-for (var i=1; i<=spirnum; i++)
+var sp_num= Math.floor(time/cycle%spirnum);
+var sp=sps[sp_num]; 
+sp.style.display="block";
+sp.style.opacity = 1;
+
+for (var i=0; i<spirnum; i++)   //clean non-current shapes
 {
-
-	if(cycletime>(i-1)*cycle && cycletime<=i*cycle)
-	{
-		var sp=sps[i-1];
-		var spnext=sps[i%spirnum];
-		if(cycletime<(i-1)*cycle +1100)
-			for (var j=1; j<=spirnum; j++)   //clean non-current shapes
-			{
-				if(i==j)
-					continue;
-				var spj=sps[j-1];
-				spj.style.display="none";
-			}
-				
-		if(cycletime>i*cycle -1200)
-        {
-        	var opacity= (cycletime-(i*cycle-1200))/1200;
-        	sp.style.opacity = 1-opacity;
-        	spnext.style.opacity = opacity;
-			spnext.style.display="block";
-			if(opacity>0.9)
-			{
-				sp.style.display="none";
-				sp.style.opacity=0;
-			}
-			if(opacity>0.95)
-				spnext.style.opacity=1;
-        }				
-	}
+	if(i==sp_num)
+	  continue;
+	var spi=sps[i];
+	spi.style.display="none";
 }
- 
+if(cycletime>(sp_num+1)*cycle -2500 || cycletime<sp_num*cycle +2500)
+{
+	var opacity;
+	if(cycletime>(sp_num+1)*cycle -2500)
+	   opacity= (cycletime-((sp_num+1)*cycle-2500))/2500;
+   else
+	   opacity= (sp_num*cycle+2500-cycletime)/2500;
+	if(opacity>0.95)
+		opacity=0.95;
+	sp.style.opacity = 1-opacity;
+}	
 
   AnimationFrameID=requestAnimFrame(function() {
           animate(startTime);
